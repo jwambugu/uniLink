f7c716f066105a9da6d65fb4f16a413274131534
Artisan <dev.tecksolke@gmail.com>
2018-02-11 10:31:38 +0300

    Created the room  model and -m

---
+++ app/Room.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Room extends Model
+{
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'roomType', 'roomCategory', 'electricityBill', 'waterBill'
+	];
+}
---
+++ database/migrations/2018_02_11_100403_create_rooms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('roomType');
+            $table->string('roomCategory');
+            $table->string('electricityBill');
+            $table->string('waterBill');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
