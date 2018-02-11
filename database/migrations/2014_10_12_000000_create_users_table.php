f7b16d3d1de09dd3a0a2ca660a6ffd85f3e8f39e
Artisan <dev.tecksolke@gmail.com>
2018-02-11 09:43:35 +0300

    Made the regNo field unique

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('regNo');
+            $table->string('regNo')->unique();
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
