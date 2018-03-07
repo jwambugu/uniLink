a7da369be997df8c603fd1560bf726ad8591baf1
ArtisanLabs <artisan254@gmail.com>
2018-03-07 10:30:32 +0300

    Changed the layout file from app to auth

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,50 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+	<!-- Top header start -->
+	@include('inc.header')
+	<!-- Top header end -->
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
+	<!-- Content area start -->
+	<div class="content-area password-reset-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<!-- Form content box start -->
+					<div class="form-content-box">
+						<!-- details -->
+						<div class="details">
+							<!-- Main title -->
+							<div class="main-title">
+								<h1><span>Reset Password</span></h1>
+							</div>
+							<!-- Form start -->
+							<form action="{{ route('password.email') }}" method="POST">
+								<div class="form-group {{ $errors->has('email') ? ' has-error' : '' }}">
+									<input type="email" name="email" class="input-text" placeholder="Email Address" >
+									@if ($errors->has('email'))
+										<span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+									@endif
+								</div>
+								<div class="form-group">
+									<button type="submit" class="button-md button-theme btn-block">Send Reset Link</button>
+								</div>
+								{{ csrf_field() }}
+							</form>
+							<!-- Form end -->
+						</div>
+						<div class="footer">
+                        <span>
+                            Remembered Password? <a href="{{ route('login') }}">Login</a>
+                        </span>
+						</div>
+					</div>
+					<!-- Form content box end -->
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Content area end -->
 @endsection
