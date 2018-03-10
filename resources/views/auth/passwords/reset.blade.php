df960dbf7ca6007cb2f67f90465d3b920accffdd
ArtisanLabs <artisan254@gmail.com>
2018-03-10 08:18:32 +0300

    Added an alert to display successful password reset

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -4,6 +4,11 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
             <div class="panel panel-default">
                 <div class="panel-heading">Reset Password</div>
 
