f67fdec1ff231b56cc9b7fdfa162e57b760b9f12
ArtisanLabs <artisan254@gmail.com>
2018-03-11 17:14:02 +0300

    Added the functions to get and update user profile

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use App\UserContact;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -25,4 +27,59 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+	/**
+	 * Here a user books a hostel
+	 * @param $id
+	 * @return \Illuminate\Http\RedirectResponse
+	 */
+	public function getHostelToBook($id){
+		return $id;
+		return redirect()->intended();
+	}
+
+	/**
+	 * Show the profile page
+	*/
+	public function getProfile(){
+
+		return view('user.profile', [
+			'contact' => AnalysisController::getContact()
+		]);
+	}
+
+	/**
+	 * Update the user profile
+	 * @param Request $request
+	 * @return Request
+	 */
+	public function updateProfile(Request $request){
+		// Validate the request
+		$this->validate($request, [
+			'name' => 'required',
+			'email' => 'required|email',
+			'phoneNumber' => 'required|unique:user_contacts',
+			'creditCard' => 'required',
+		]);
+
+		// Update the name and username
+		$user = User::find(auth()->user()->id);
+
+		$user->name = $request['name'];
+		$user->email = $request['email'];
+
+		$user->update();
+
+		// Update contact data
+		$contact = AnalysisController::getContact();
+
+		$contact->phoneNumber = $request['phoneNumber'];
+		$contact->creditCard = $request['creditCard'];
+
+		$contact->update();
+
+		alert()->success('Profile data successfully updated.', 'Update Complete')->autoclose(3000);
+
+		return redirect()->route('home');
+	}
 }
