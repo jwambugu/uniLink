e0aa1572f4b62c67ea7f67bcaf00e32773290226
ArtisanLabs <artisan254@gmail.com>
2018-03-14 17:20:26 +0300

    Added the users hostels to the view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,116 +1,47 @@
 @extends('layouts.auth')
 
 @section('content')
-    @include('inc.navbar')
-    <!-- My Propertiess start -->
-    <div class="content-area my-properties">
-	    <div class="container">
-		    <div class="row">
-			    <div class="col-lg-4 col-md-4 col-sm-12">
-				    <!-- User account box start -->
-				    @include('inc.sideNav')
-				    <!-- User account box end -->
-			    </div>
+	@include('inc.navbar')
+	<!-- My Properties start -->
+	<div class="content-area my-properties">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 col-md-4 col-sm-12">
+					<!-- User account box start -->
+				@include('inc.sideNav')
+				<!-- User account box end -->
+				</div>
 
-			    <div class="col-lg-8 col-md-8 col-sm-12">
-				    <div class="main-title-2">
-					    <h1><span>My</span> Properties</h1>
-				    </div>
-				    <!-- table start -->
-				    <table class="manage-table responsive-table">
-					    <tbody>
+				<div class="col-lg-8 col-md-8 col-sm-12">
+					<div class="main-title-2"><br>
+						<h1><span>My</span> Hostels</h1>
+					</div>
+					<!-- table start -->
+					<table class="manage-table responsive-table">
+						<tbody>
 
-					    <tr>
-						    <td class="title-container">
-							    <img src="img/my-properties/my-properties-1.jpg" alt="my-properties-1" class="img-responsive hidden-xs">
-							    <div class="title">
-								    <h4><a href="#">beautiful  Family  home </a></h4>
-								    <span><i class="fa fa-map-marker"></i> 123 Kathal St. Tampa City, </span>
-								    <span class="table-property-price">$900 / monthly</span>
-							    </div>
-						    </td>
-						    <td class="expire-date hidden-xs">December 17 2017</td>
-						    <td class="action">
-							    <a href="#"><i class="fa fa-pencil"></i> Edit</a>
-							    <a href="#"><i class="fa  fa-eye-slash"></i> Hide</a>
-							    <a href="#" class="delete"><i class="fa fa-remove"></i> Delete</a>
-						    </td>
-					    </tr>
-
-					    <tr>
-						    <td class="title-container">
-							    <img src="img/my-properties/my-properties-2.jpg" alt="my-properties-5" class="img-responsive hidden-xs">
-							    <div class="title">
-								    <h4><a href="#">beautiful  Family  home </a></h4>
-								    <span><i class="fa fa-map-marker"></i> 123 Kathal St. Tampa City, </span>
-								    <span class="table-property-price">$900 / monthly</span>
-							    </div>
-						    </td>
-						    <td class="expire-date hidden-xs">December 17 2017</td>
-						    <td class="action">
-							    <a href="#"><i class="fa fa-pencil"></i> Edit</a>
-							    <a href="#"><i class="fa  fa-eye-slash"></i> Hide</a>
-							    <a href="#" class="delete"><i class="fa fa-remove"></i> Delete</a>
-						    </td>
-					    </tr>
-
-					    <tr>
-						    <td class="title-container">
-							    <img src="img/my-properties/my-properties-3.jpg" alt="my-properties-3" class="img-responsive hidden-xs">
-							    <div class="title">
-								    <h4><a href="#">beautiful  Family  home </a></h4>
-								    <span><i class="fa fa-map-marker"></i> 123 Kathal St. Tampa City, </span>
-								    <span class="table-property-price">$900 / monthly</span>
-							    </div>
-						    </td>
-						    <td class="expire-date hidden-xs">December 17 2017</td>
-						    <td class="action">
-							    <a href="#"><i class="fa fa-pencil"></i> Edit</a>
-							    <a href="#"><i class="fa  fa-eye-slash"></i> Hide</a>
-							    <a href="#" class="delete"><i class="fa fa-remove"></i> Delete</a>
-						    </td>
-					    </tr>
-
-					    <tr>
-						    <td class="title-container">
-							    <img src="img/my-properties/my-properties-4.jpg" alt="my-properties-4" class="img-responsive hidden-xs">
-							    <div class="title">
-								    <h4><a href="#">beautiful  Family  home </a></h4>
-								    <span><i class="fa fa-map-marker"></i> 123 Kathal St. Tampa City, </span>
-								    <span class="table-property-price">$900 / monthly</span>
-							    </div>
-						    </td>
-						    <td class="expire-date hidden-xs">December 17 2017</td>
-						    <td class="action">
-							    <a href="#"><i class="fa fa-pencil"></i> Edit</a>
-							    <a href="#"><i class="fa  fa-eye-slash"></i> Hide</a>
-							    <a href="#" class="delete"><i class="fa fa-remove"></i> Delete</a>
-						    </td>
-					    </tr>
-
-					    <tr>
-						    <td class="title-container">
-							    <img src="img/my-properties/my-properties-5.jpg" alt="my-properties-5" class="img-responsive hidden-xs">
-							    <div class="title">
-								    <h4><a href="#">beautiful  Family  home </a></h4>
-								    <span><i class="fa fa-map-marker"></i> 123 Kathal St. Tampa City, </span>
-								    <span class="table-property-price">$900 / monthly</span>
-							    </div>
-						    </td>
-						    <td class="expire-date hidden-xs">December 17 2017</td>
-						    <td class="action">
-							    <a href="#"><i class="fa fa-pencil"></i> Edit</a>
-							    <a href="#"><i class="fa  fa-eye-slash"></i> Hide</a>
-							    <a href="#" class="delete"><i class="fa fa-remove"></i> Delete</a>
-						    </td>
-					    </tr>
-
-					    </tbody>
-				    </table>
-				    <!-- table end -->
-			    </div>
-		    </div>
-	    </div>
-    </div>
-    <!-- My Propertiess end -->
+						@foreach($images as $image)
+						@endforeach
+						@foreach($myHostels as $myHostel)
+							<tr>
+								<td class="title-container">
+									<img src="{{ asset('storage/hostel_images') }}/{{ $image->image }}" alt="my-properties-1" class="img-responsive hidden-xs">
+									<div class="title">
+										<h4><a href="{{ url('/view') }}/{{ $myHostel->hostel->id }}">{{ $myHostel->hostel->name }}</a></h4>
+										<span><i class="fa fa-phone"></i> {{ $myHostel->hostel->contact }}</span>
+										<span class="table-property-price">Rent Amount: KES {{ number_format($myHostel->hostel->price) }} / per sem</span>
+										<br>
+										<span class="table-property-price">Booked On: {{ date('F d, Y', strtotime($myHostel->created_at)) }}</span>
+									</div>
+								</td>
+							</tr>
+						@endforeach
+						</tbody>
+					</table>
+					<!-- table end -->
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- My Properties end -->
 @endsection
