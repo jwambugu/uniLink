2895c8fe3b423b597bc9563b7d31eeea4ec7b387
ArtisanLabs <artisan254@gmail.com>
2018-03-22 21:19:48 +0300

    Created the invoice and test templates

--- resources/views/emails/invoice.blade.php
+++ resources/views/emails/invoice.blade.php
@@ -1,7 +1,60 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: artisan
- * Date: 3/22/18
- * Time: 7:54 PM
- */
+@extends('beautymail::templates.minty')
+
+@section('content')
+
+	@include('beautymail::templates.minty.contentStart', ['color' => '#3c8dbc'])
+	<tr>
+		<td class="title">
+			<strong>
+				Thanks for your order {{ $name }}, here's your invoice.
+			</strong>
+		</td>
+	</tr>
+	<tr>
+		<td width="100%" height="10"></td>
+	</tr>
+	<tr>
+		<td class="paragraph">
+			You have received this email because you have booked a room using UniLink. We are happy you chose us.
+		</td>
+	</tr>
+	<tr>
+		<td width="100%" height="25"></td>
+	</tr>
+	<tr>
+		<td class="title">
+			Booking Details
+		</td>
+	<tr>
+		<td width="100%" height="10"></td>
+	</tr>
+	<tr>
+		<td class="paragraph">
+			<strong>Hostel name</strong> - {{ $hostel }}
+		</td>
+	</tr>
+	<tr>
+		<td class="paragraph">
+			<strong>Hostel Rent</strong> - KES {{ number_format($price) }}
+		</td>
+	</tr>
+	<tr>
+		<td class="paragraph">
+			<strong>Stripe ID</strong> - {{ $stripeID }}
+		</td>
+	</tr>
+	<tr>
+		<td class="paragraph">
+			<strong>Total Amount - KES {{ number_format($price) }}</strong>
+		</td>
+	</tr>
+	<tr>
+		<td width="100%" height="25"></td>
+	</tr>
+
+	<tr>
+		<td width="100%" height="25"></td>
+	</tr>
+	@include('beautymail::templates.minty.contentEnd')
+
+@stop
--- resources/views/emails/testSend.blade.php
+++ resources/views/emails/testSend.blade.php
@@ -1,7 +1,64 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: artisan
- * Date: 3/22/18
- * Time: 5:52 PM
- */
+@extends('beautymail::templates.minty')
+
+@section('content')
+
+	@include('beautymail::templates.minty.contentStart', ['color' => '#3c8dbc'])
+	<tr>
+		<td class="title">
+			<strong>
+				Thanks for your order {{ $name }}, here's your invoice.
+			</strong>
+		</td>
+	</tr>
+	<tr>
+		<td width="100%" height="10"></td>
+	</tr>
+	<tr>
+		<td class="paragraph">
+			You have received this email because you have booked a room using UniLink. We are happy you chose us.
+		</td>
+	</tr>
+	<tr>
+		<td width="100%" height="25"></td>
+	</tr>
+	<tr>
+		<td class="title">
+			Booking Details
+		</td>
+	<tr>
+		<td width="100%" height="10"></td>
+	</tr>
+	<tr>
+		<td class="paragraph">
+			<strong>Hostel name</strong> - {{ $name }}
+		</td>
+	</tr>
+	<tr>
+		<td class="paragraph">
+			<strong>Hostel Rent</strong> - KES {{ $name }}
+		</td>
+	</tr>
+	<tr>
+		<td class="paragraph">
+			<strong>Stripe ID</strong> - {{ $name }}
+		</td>
+	</tr>
+	<tr>
+		<td class="paragraph">
+			<strong>Total Amount - KES {{ $name }}</strong>
+		</td>
+	</tr>
+	<tr>
+		<td width="100%" height="25"></td>
+	</tr>
+	{{--<tr>--}}
+		{{--<td>--}}
+			{{--@include('beautymail::templates.minty.button', ['text' => 'Sign in', 'link' => '#'])--}}
+		{{--</td>--}}
+	{{--</tr>--}}
+	<tr>
+		<td width="100%" height="25"></td>
+	</tr>
+	@include('beautymail::templates.minty.contentEnd')
+
+@stop
