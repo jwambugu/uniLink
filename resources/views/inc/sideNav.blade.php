44e3559c0dbde445782a68534f1fba1bc5e7abf0
Artisan <artisan254@gmail.com>
2018-05-04 08:58:18 +0300

    Mapped the logout link to the default logout route

--- resources/views/inc/sideNav.blade.php
+++ resources/views/inc/sideNav.blade.php
@@ -4,12 +4,12 @@
 		<p>{{ auth()->user()->email }}</p>
 		<div class="edit-profile-photo">
 			<img src="{{ asset('nest/img/avatar/avatar-5.png') }}" alt="agent-1" class="img-responsive">
-			<div class="change-photo-btn">
-				<div class="photoUpload">
-					<span><i class="fa fa-upload"></i> Upload Photo</span>
-					<input type="file" class="upload">
-				</div>
-			</div>
+			{{--<div class="change-photo-btn">--}}
+				{{--<div class="photoUpload">--}}
+					{{--<span><i class="fa fa-upload"></i> Upload Photo</span>--}}
+					{{--<input type="file" class="upload">--}}
+				{{--</div>--}}
+			{{--</div>--}}
 		</div>
 	</div>
 	<div class="content">
@@ -30,9 +30,15 @@
 				</a>
 			</li>
 			<li>
-				<a href="{{ route('logout') }}">
-					<i class="flaticon-sign-out-option"></i>Log Out
+				<a class="dropdown-item" href="{{ route('logout') }}"
+				   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+					<i class="flaticon-sign-out-option"></i> Logout
 				</a>
+
+				<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+					@csrf
+				</form>
 			</li>
 		</ul>
 	</div>
